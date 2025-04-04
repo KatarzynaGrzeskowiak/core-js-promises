25789ab6ed259fb3521269d2896900eb60897a9f
Katarzyna Grześkowiak <tabita33@gmail.com>
2025-04-04 22:32:25 +0200

    feat: quequPromises fn

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -142,8 +142,22 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+async function queuPromises(promises) {
+  let result = '';
+
+  await promises.reduce(async (previousPromise, currentPromise) => {
+    await previousPromise;
+    try {
+      const value = await currentPromise;
+      if (value !== undefined) {
+        result += value;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, Promise.resolve());
+
+  return result;
 }
 
 module.exports = {
